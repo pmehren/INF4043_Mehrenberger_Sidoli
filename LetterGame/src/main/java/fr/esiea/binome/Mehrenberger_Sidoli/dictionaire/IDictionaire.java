438c4e537d36ea809997dbc0384d12bb58fc7975
Sundride <raphael.sidoli@gmail.com>
2017-03-03 14:19:08 +0100

    Update IDictionaire.java

--- LetterGame/src/main/java/fr/esiea/binome/Mehrenberger_Sidoli/dictionaire/IDictionaire.java
+++ LetterGame/src/main/java/fr/esiea/binome/Mehrenberger_Sidoli/dictionaire/IDictionaire.java
@@ -4,10 +4,6 @@ package fr.esiea.binome.Mehrenberger_Sidoli.dictionaire;
  * Created on 16/01/17.
  */
 public interface IDictionaire {
-<<<<<<< HEAD
     boolean verifyWord(String word);
-=======
-    boolean isWord(String word);
->>>>>>> 983f5c274c2ad13192fccf87a066f2cdff01cf7b
 }
 
