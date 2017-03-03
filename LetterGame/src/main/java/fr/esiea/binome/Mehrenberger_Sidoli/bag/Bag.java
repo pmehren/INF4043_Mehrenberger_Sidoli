878646fcd9f1b1f457f6e7b033bbf52833cd81a1
Sundride <raphael.sidoli@gmail.com>
2017-03-03 14:13:32 +0100

    Pb de Head?

--- LetterGame/src/main/java/fr/esiea/binome/Mehrenberger_Sidoli/bag/Bag.java
+++ LetterGame/src/main/java/fr/esiea/binome/Mehrenberger_Sidoli/bag/Bag.java
@@ -13,7 +13,6 @@ public class Bag {
 		this.setListe(liste);
 	}
 	
-<<<<<<< HEAD
 	public List<Letter> getListe() {
 		return liste;
 	}
@@ -22,8 +21,6 @@ public class Bag {
 		this.liste = liste;
 	}
 	
-=======
->>>>>>> 983f5c274c2ad13192fccf87a066f2cdff01cf7b
 	public Letter getPremiereLettre(List<Letter> liste) {
 		return liste.get(0);
 	}
@@ -32,21 +29,14 @@ public class Bag {
 		return liste.get(1);
 	}
 	
-<<<<<<< HEAD
 	public void initListeBag(List<Letter> liste){ //Permet de réguler l'apparition des voyelles et consonnes
-=======
-	public void initListeBag(List<Letter> liste){
->>>>>>> 983f5c274c2ad13192fccf87a066f2cdff01cf7b
 		for(int i=0; i<26; i++) {
 			if(i == 0 || i == 4 || i == 8 || i == 14 || i == 20 || i == 24) //Si c'est une voyelle
 			{
 				for(int j=0; j<3; j++) {
-<<<<<<< HEAD
 					Letter l = new Letter((char)(i+97), i, 3); //+97 correspond à l'alphabet (minuscules) en table ascii.
 					//On initialise le nbOccurence des voyelles à 4 afin qu'il y est 4fois plus de voyelles que de consonnes.
-=======
 					Letter l = new Letter((char)(i+97), i, 4); //+97 correspond à l'alphabet (minuscules) en table ascii
->>>>>>> 983f5c274c2ad13192fccf87a066f2cdff01cf7b
 					liste.add(l);
 				}
 			}
@@ -54,9 +44,6 @@ public class Bag {
 			liste.add(l);
 		}
 	}
-<<<<<<< HEAD
-		
-=======
 	
 	public int getTailleBag(List<Letter> liste) {
 		return liste.size();
@@ -66,7 +53,6 @@ public class Bag {
 		return liste.get(index);
 	}
 	
->>>>>>> 983f5c274c2ad13192fccf87a066f2cdff01cf7b
 	public List<Character> getBagListe(List<Letter> liste) {
 				List<Character> valeurListe = new ArrayList<>();
 				
@@ -74,22 +60,12 @@ public class Bag {
 					valeurListe.add(liste.get(i).getValeur());
 				}	
 			return valeurListe;	
-	}
-<<<<<<< HEAD
-	
-	
-			
+	}	
 	
 	public void melangeListe(List<Letter> liste) { //Permet de mélanger la liste de façon aléatoire
 		Collections.shuffle(liste);
 	}
-=======
 			
-	
-	public void melangeListe(List<Letter> liste) {
-		Collections.shuffle(liste);
-	}
-		
 	public List<Letter> getListe() {
 		return liste;
 	}
@@ -97,5 +73,4 @@ public class Bag {
 	public void setListe(List<Letter> liste) {
 		this.liste = liste;
 	}
->>>>>>> 983f5c274c2ad13192fccf87a066f2cdff01cf7b
 }
